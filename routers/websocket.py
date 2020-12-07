9e6eb803d314be0477ec4e9057a24ad076af5091
Ander813 <gegorike@mail.ru>
2020-12-07 20:56:19 +0300

    added group_subscribe method to broadcater

--- routers/websocket.py
+++ routers/websocket.py
@@ -10,24 +10,41 @@ broadcast = Broadcast('memory://')
 
 
 class Chat(WebSocketEndpoint):
+
     async def on_connect(self, websocket: WebSocket) -> None:
         await websocket.accept()
-        await run_until_first_complete(
-            (self.chatroom_ws_receiver, {"websocket": websocket}),
-            (self.chatroom_ws_sender, {"websocket": websocket}),
-        )
+        if int(self.scope['path_params']['id']) == 1:
+            await run_until_first_complete(
+                (self.receive_, {"websocket": websocket}),
+                (self.send_, {"websocket": websocket}),
+            )
+        else:
+            await run_until_first_complete(
+                (self.receive_, {"websocket": websocket}),
+                (self.test, {"websocket": websocket}),
+            )
 
-    async def chatroom_ws_receiver(self, websocket):
+    async def receive_(self, websocket):
+        if int(self.scope['path_params']['id']) == 1:
+            c = 'chatroom'
+        else:
+            c = 'puk'
         async for message in websocket.iter_text():
-            await broadcast.publish(channel="chatroom", message=message)
+            await broadcast.publish(channel=c, message=message)
 
-    async def chatroom_ws_sender(self, websocket):
+    async def send_(self, websocket):
         async with broadcast.subscribe(channel="chatroom") as subscriber:
             async for event in subscriber:
                 await websocket.send_text(event.message)
 
+    async def test(self, websocket):
+        sub = await broadcast.group_subscribe(['chatroom', 'g1', 'g2'])
+        async for event in sub:
+            print(self.scope['path_params']['id'])
+            await websocket.send_text(event.message)
 
-routes = [WebSocketRoute('/chat', Chat)]
+
+routes = [WebSocketRoute('/chat/{id}', Chat)]
 router = APIRouter(routes,
                    on_startup=[broadcast.connect],
                    on_shutdown=[broadcast.disconnect])
