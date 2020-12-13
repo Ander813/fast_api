9f80c3af6456fe294e5e633624a4c1aa9f8945a8
Ander813 <gegorike@mail.ru>
2020-12-13 21:01:21 +0300

    Added CORS and Session middleware

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from starlette.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 from src.conf import settings
 from src.app.router import main_router
 
-app = FastAPI()
 
+middleware = [
+    Middleware(CORSMiddleware,
+               allow_origins=["*"],
+               allow_credentials=True,
+               allow_methods=["*"],
+               allow_headers=["*"]),
+    Middleware(SessionMiddleware,
+               secret_key=settings.SECRET_KEY,
+               same_site='None')
+]
+
+app = FastAPI(middleware=middleware)
 
 app.include_router(main_router,
                    prefix="/api/v1")
