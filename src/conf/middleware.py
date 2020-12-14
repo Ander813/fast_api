0ed564b8ecae10784a846e0e8114d6e994b8ce5a
Ander813 <gegorike@mail.ru>
2020-12-14 12:47:46 +0300

    Small refactor: moved middleware to conf.middleware

---
+++ src/conf/middleware.py
@@ -0,0 +1,16 @@
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from . import settings
+
+
+middleware = [
+    Middleware(CORSMiddleware,
+               allow_origins=["*"],
+               allow_credentials=True,
+               allow_methods=["*"],
+               allow_headers=["*"]),
+    Middleware(SessionMiddleware,
+               secret_key=settings.SECRET_KEY,
+               same_site='None')
+]
