c06f9fb8d8bca5563e7869d8d1eebfa104d4fa7f
Ander813 <gegorike@mail.ru>
2020-12-23 14:49:16 +0300

    Changed admin schema name

--- src/app/records/schemas.py
+++ src/app/records/schemas.py
@@ -4,4 +4,4 @@ from .models import Record
 
 RecordOut = pydantic_model_creator(Record, name='record_out', exclude=('creator', 'creator_id'))
 RecordIn = pydantic_model_creator(Record, name='record_in', exclude_readonly=True)
-RecordOutAdmin = pydantic_model_creator(Record, name='record_out_in', exclude=('creator',))
+RecordOutAdmin = pydantic_model_creator(Record, name='record_out_admin', exclude=('creator',))
