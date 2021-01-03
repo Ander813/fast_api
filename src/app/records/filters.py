0df8d0b896747b26b6dbcc4b658016f59e8d29b1
Ander813 <gegorike@mail.ru>
2021-01-03 17:53:17 +0300

    Fixed is_important parameter filtering

--- src/app/records/filters.py
+++ src/app/records/filters.py
@@ -7,11 +7,13 @@ from src.app.base.filters import BaseFilter
 
 
 class RecordFilter(BaseFilter):
-    def __init__(self,
-                 is_important: Optional[str] = Query(None),
-                 date_from: Optional[datetime] = Query(None),
-                 date_to: Optional[datetime] = Query(datetime.now()),
-                 order: Optional[str] = Query(None)):
+    def __init__(
+        self,
+        is_important: Optional[bool] = Query(None),
+        date_from: Optional[datetime] = Query(None),
+        date_to: Optional[datetime] = Query(datetime.now()),
+        order: Optional[str] = Query(None),
+    ):
         self.is_important = is_important
         self.edit_date__gte = date_from
         self.edit_date__lte = date_to
@@ -19,12 +21,14 @@ class RecordFilter(BaseFilter):
 
 
 class RecordAdminFilter(BaseFilter):
-    def __init__(self,
-                 is_important: Optional[str] = Query(None),
-                 date_from: Optional[datetime] = Query(None),
-                 date_to: Optional[datetime] = Query(datetime.now()),
-                 order: Optional[str] = Query(None),
-                 creator_id: Optional[int] = Query(None)):
+    def __init__(
+        self,
+        is_important: Optional[str] = Query(None),
+        date_from: Optional[datetime] = Query(None),
+        date_to: Optional[datetime] = Query(datetime.now()),
+        order: Optional[str] = Query(None),
+        creator_id: Optional[int] = Query(None),
+    ):
         self.creator_id = creator_id
         self.is_important = is_important
         self.edit_date__gte = date_from
