08f4c464cdb7082e247469deaa297aff0ed9f0e0
Ander813 <gegorike@mail.ru>
2021-01-04 17:45:40 +0300

    Fully covered records routes with tests

--- src/app/records/tests/test_routes.py
+++ src/app/records/tests/test_routes.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 
 from httpx import AsyncClient
 from tortoise.contrib.test import initializer, finalizer
@@ -39,6 +40,7 @@ async def create_user():
 
 async def create_records(id: int):
     for i in range(len(records)):
+        await asyncio.sleep(0.1)
         await records_s.create(records[i], creator_id=id)
 
 
@@ -112,7 +114,7 @@ async def test_get_records_with_filter_params():
         )
     assert response.status_code == 400
 
-    """Testing with -id parameter"""
+    """Testing with -id order parameter"""
     async with client as c:
         response = await c.get(
             app.url_path_for("get_records"),
@@ -124,7 +126,7 @@ async def test_get_records_with_filter_params():
     for i in range(len(records)):
         assert json_resp["items"][i]["id"] == len(records) - i
 
-    """Testing with id parameter"""
+    """Testing with id order parameter"""
     async with client as c:
         response = await c.get(
             app.url_path_for("get_records"),
@@ -161,6 +163,194 @@ async def test_get_records_with_filter_params():
     assert len(json_resp["items"]) == len(records) // 2
     for i in range(len(json_resp["items"])):
         assert json_resp["items"][i]["is_important"] is False
+
+    """Testing with is_important + order parameters"""
+    async with client as c:
+        response = await c.get(
+            app.url_path_for("get_records"),
+            headers=authorization,
+            params={"is_important": "1", "order": "id"},
+        )
+    assert response.status_code == 200
+    json_resp = json.loads(response.content)
+    assert len(json_resp["items"]) == len(records) // 2 + 1
+    for i in range(len(json_resp["items"]) - 1):
+        assert json_resp["items"][i]["id"] < json_resp["items"][i + 1]["id"]
+
+    """Testing with date_from = now() => expecting 0 items"""
+    async with client as c:
+        response = await c.get(
+            app.url_path_for("get_records"),
+            headers=authorization,
+            params={"date_from": datetime.now()},
+        )
+    assert response.status_code == 200
+    json_resp = json.loads(response.content)
+    assert len(json_resp["items"]) == 0
+
+    """Testing with date_to = 3rd record creation time"""
+    record = await records_s.get(id=3)
+    time_created = record.create_date
+    async with client as c:
+        response = await c.get(
+            app.url_path_for("get_records"),
+            headers=authorization,
+            params={"date_to": time_created},
+        )
+    assert response.status_code == 200
+    json_resp = json.loads(response.content)
+    assert len(json_resp["items"]) == 3
+
+
+@pytest.mark.asyncio()
+async def test_get_record_unauthorized():
+    async with client as c:
+        response = await c.get(app.url_path_for("get_record", id="1"))
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio()
+async def test_get_record_authorized_with_bad_id():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.get(
+            app.url_path_for("get_record", id="bad_id"), headers=authorization
+        )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio()
+async def test_get_record_authorized_with_wrong_id():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.get(
+            app.url_path_for("get_record", id=len(records) + 1), headers=authorization
+        )
+    assert response.status_code == 404
+
+
+@pytest.mark.asyncio()
+async def test_get_record_authorized():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.get(
+            app.url_path_for("get_record", id=1), headers=authorization
+        )
+    assert response.status_code == 200
+    json_resp = json.loads(response.content)
+    for key in RecordOut.__fields__:
+        assert key in json_resp
+    assert json_resp["id"] == 1
+
+
+@pytest.mark.asyncio()
+async def test_update_record_unauthorized():
+    async with client as c:
+        response = await c.put(
+            app.url_path_for("update_record", id=1), data=records[1].json()
+        )
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio()
+async def test_update_record_authorized_with_bad_id():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.put(
+            app.url_path_for("update_record", id="dab_id"),
+            data=records[1].json(),
+            headers=authorization,
+        )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio()
+async def test_update_record_authorized_with_wrong_id():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.put(
+            app.url_path_for("update_record", id=len(records) + 1),
+            data=records[1].json(),
+            headers=authorization,
+        )
+    assert response.status_code == 404
+
+
+@pytest.mark.asyncio()
+async def test_update_record_authorized():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.put(
+            app.url_path_for("update_record", id=1),
+            data=records[1].json(),
+            headers=authorization,
+        )
+    assert response.status_code == 200
+    json_resp = json.loads(response.content)
+    for key in RecordOut.__fields__:
+        assert key in json_resp
+    for key in RecordIn.__fields__:
+        assert json_resp[key] == records[1].dict()[key]
+
+
+@pytest.mark.asyncio()
+async def test_delete_record_unauthorized():
+    async with client as c:
+        response = await c.delete(app.url_path_for("delete_record", id=1))
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio()
+async def test_delete_record_authorized_with_bad_id():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.delete(
+            app.url_path_for("delete_record", id="bad_id"), headers=authorization
+        )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio()
+async def test_delete_record_authorized_with_wrong_id():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.delete(
+            app.url_path_for("delete_record", id=len(records) + 1),
+            headers=authorization,
+        )
+    assert response.status_code == 404
+
+
+@pytest.mark.asyncio()
+async def test_delete_record_authorized():
+    await create_user()
+    await create_records(id=1)
+
+    async with client as c:
+        response = await c.delete(
+            app.url_path_for("delete_record", id=1),
+            headers=authorization,
+        )
+    assert response.status_code == 200
+    records_in_db = await records_s.all()
+    assert len(records_in_db) == len(records) - 1
+    assert await records_s.filter(id=1) == []
 
 
 # initializer(settings.APPS_MODELS)
--- testing_main.py
+++ testing_main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from src.conf import settings
+
 from src.app.router import main_router
 from src.app.admin_router import admin_router
+from src.conf import settings
 from src.conf.middleware import middleware
 from src.conf.exceptions import exceptions
 
@@ -12,3 +13,11 @@ app = FastAPI(middleware=middleware, exception_handlers=exceptions)
 app.include_router(main_router, prefix="/api/v1")
 
 app.include_router(admin_router, prefix="/admin")
+
+register_tortoise(
+    app=app,
+    modules={"models": settings.APPS_MODELS},
+    db_url="sqlite://:memory:",
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
