ef2765796526002557c37ad7c54443b5bef56555
Ander813 <gegorike@mail.ru>
2021-01-04 17:00:49 +0300

    Changed pk -> id in routes

--- src/app/records/routes.py
+++ src/app/records/routes.py
@@ -15,46 +15,45 @@ from src.app.users.models import User
 router = APIRouter()
 
 
-@router.get('/',
-            response_model=Page[RecordOut],
-            dependencies=[Depends(pagination_params)],
-            responses={400: {'detail': 'Bad queryset'}})
-async def get_records(user: User = Depends(get_current_user),
-                      filter_params: RecordFilter = Depends()):
+@router.get(
+    "/",
+    response_model=Page[RecordOut],
+    dependencies=[Depends(pagination_params)],
+    responses={400: {"detail": "Bad queryset"}},
+)
+async def get_records(
+    user: User = Depends(get_current_user), filter_params: RecordFilter = Depends()
+):
     queryset = await records_s.filter_queryset(filter_params, creator_id=user.id)
     if queryset is None:
-        raise HTTPException(status_code=400, detail='wrong parameters given')
+        raise HTTPException(status_code=400, detail="wrong parameters given")
     return await paginate(queryset)
 
 
-@router.get('/{id}',
-            response_model=RecordOut,
-            responses={404: {"model": HTTPNotFoundError}})
+@router.get(
+    "/{id}", response_model=RecordOut, responses={404: {"model": HTTPNotFoundError}}
+)
 async def get_record(id: int, user: User = Depends(get_current_user)):
     return await records_s.get(id=id, creator_id=user.id)
 
 
-@router.post('/',
-             response_model=RecordOut,
-             status_code=201)
-async def create_record(record: RecordIn,
-                        user: User = Depends(get_current_user)):
+@router.post("/", response_model=RecordOut, status_code=201)
+async def create_record(record: RecordIn, user: User = Depends(get_current_user)):
     return await records_s.create(record, creator_id=user.id)
 
 
-@router.put('/{pk}',
-            responses={404: {"description": "not found"}})
-async def update_record(pk: int, record: RecordIn,
-                        user: User = Depends(get_current_user)):
-    return await records_s.update(record, id=pk, creator_id=user.id)
+@router.put("/{id}", responses={404: {"description": "not found"}})
+async def update_record(
+    id: int, record: RecordIn, user: User = Depends(get_current_user)
+):
+    return await records_s.update(record, id=id, creator_id=user.id)
 
 
-@router.delete('/{pk}',
-               response_model=Msg,
-               responses={404: {'description': 'not found'}})
-async def delete_record(pk: int,
-                        user: User = Depends(get_current_user)):
-    if await records_s.delete(id=pk, creator_id=user.id):
-        return {'msg': 'deleted'}
+@router.delete(
+    "/{id}", response_model=Msg, responses={404: {"description": "not found"}}
+)
+async def delete_record(id: int, user: User = Depends(get_current_user)):
+    if await records_s.delete(id=id, creator_id=user.id):
+        return {"msg": "deleted"}
     else:
-        raise HTTPException(status_code=404, detail='object not found')
+        raise HTTPException(status_code=404, detail="object not found")
