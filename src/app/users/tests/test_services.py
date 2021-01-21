c22a53d988ece32b6d80e783edcc6e937853c2ee
Ander813 <gegorike@mail.ru>
2021-01-21 15:56:50 +0300

    Started tests for user services

---
+++ src/app/users/tests/test_services.py
@@ -0,0 +1,26 @@
+import pytest
+from tortoise.contrib.test import initializer, finalizer
+
+from src.app.users.models import User
+from src.app.users.schemas import UserIn
+from src.app.users.services import users_s
+from src.conf import settings
+
+
+user_email = "test@mail.ru"
+user_password = "12345"
+user_schema = UserIn(email=user_email, password=user_password)
+
+
+@pytest.fixture(autouse=True)
+def initialize_tests(request):
+    initializer(settings.APPS_MODELS)
+    request.addfinalizer(finalizer)
+
+
+@pytest.mark.asyncio
+async def test_create():
+    user = await users_s.create(user_schema)
+    user_obj = await User.get_or_none(id=1)
+    assert user_obj.verify_password(user_password)
+    assert user_obj.email == user_email
